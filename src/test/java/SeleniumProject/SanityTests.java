3d1b2853666c258862b166f92953b2f696b1d4e9
DeanAvram <dean6198@gmail.com>
2023-05-01 14:30:20 +0300

    Update SuccessfulCantProccedToCheckout test

--- src/test/java/SeleniumProject/SanityTests.java
+++ src/test/java/SeleniumProject/SanityTests.java
@@ -23,6 +23,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+
 import java.util.*;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -38,7 +40,7 @@ import org.apache.log4j.SimpleLayout;*/
 public class SanityTests {
 	private static WebDriver driver;
 	private Map<String, Object> vars;
-	private static String pattern = "[^\\d.]"; //everything part of numbers and dots
+	private static String pattern = "[^\\d.]"; // everything part of numbers and dots
 	public static int countSuccessfulPurchaseOperation = 0;
 	public static int countUnsuccessfulPurchaseOperation = 0;
 	public static int countSuccessfulAddToCart = 0;
@@ -57,32 +59,32 @@ public class SanityTests {
 		vars = new HashMap<String, Object>();
 
 	}
-	
-	public static void printMethodName () {
-		System.out.println("Starting " +  Thread.currentThread().getStackTrace()[2].getMethodName());
+
+	public static void printMethodName() {
+		System.out.println("Starting " + Thread.currentThread().getStackTrace()[2].getMethodName());
 	}
-	
-	
-	
-	public static double getCartAmount () {
-		String s = driver.findElement(By.xpath("//*[@id=\"ast-site-header-cart\"]/div[1]/a/span/span/span/bdi")).getText();
-		return  Double.parseDouble(s.replaceAll(pattern, ""));
+
+	public static double getCartAmount() {
+		String s = driver.findElement(By.xpath("//*[@id=\"ast-site-header-cart\"]/div[1]/a/span/span/span/bdi"))
+				.getText();
+		return Double.parseDouble(s.replaceAll(pattern, ""));
 	}
 
 	@After
 	public void tearDown() {
-		// driver.quit();
+		driver.quit();
 	}
 
-	//@Test
+	@Test
 	public void SuccessfulPurchaseOperation() throws InterruptedException {
+		printMethodName();
 		driver.get("https://atid.store/");
 		driver.manage().window().setSize(new Dimension(1052, 666));
 		driver.findElement(By.id("menu-item-45")).click();
 		driver.findElement(By.xpath("//*[@id=\"main\"]/div/ul/li[1]/div[1]/a/img")).click();
 		driver.findElement(By.name("add-to-cart")).click();
 		driver.findElement(By.className("ast-site-header-cart")).click();
-		
+
 		driver.findElement(By.className("wc-proceed-to-checkout")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.id("billing_first_name")).sendKeys("Israel");
@@ -94,37 +96,36 @@ public class SanityTests {
 		driver.findElement(By.id("billing_email")).sendKeys("mail@gmail.com");
 		driver.findElement(By.id("place_order")).click();
 		Thread.sleep(3000);
-		//List<WebElement> l =  driver.findElements(By.className("woocommerce-error"));
+		// List<WebElement> l = driver.findElements(By.className("woocommerce-error"));
 		if (driver.findElements(By.className("woocommerce-error")).isEmpty()) {
-			//did not find an error message error
+			// did not find an error message error
 			countSuccessfulPurchaseOperation++;
 		}
 
 	}
-	
-	/*
-	 @Test 
-	 public void UnsuccessfulPurchaseOperation() throws InterruptedException {
-		 driver.get("https://atid.store/");
-			driver.manage().window().setSize(new Dimension(1052, 666));
-			driver.findElement(By.id("menu-item-45")).click();
-			driver.findElement(By.xpath("//*[@id=\"main\"]/div/ul/li[1]/div[1]/a/img")).click();
-			driver.findElement(By.name("add-to-cart")).click();
-			driver.findElement(By.className("ast-site-header-cart")).click();
-			driver.findElement(By.className("wc-proceed-to-checkout")).click();
-			Thread.sleep(1000);
-			driver.findElement(By.id("place_order")).click();
-			Thread.sleep(3000);
-			//List<WebElement> l =  driver.findElements(By.className("woocommerce-error"));
-			if (!driver.findElements(By.className("woocommerce-error")).isEmpty()) {
-				// found at least one error (we wanted to see those error)
-				countUnsuccessfulPurchaseOperation++;
-			}
-	 }*/
-	
-	
-	//@Test 
-	 public void SuccessfulAddToCartOperation() throws InterruptedException {
+
+	@Test
+	public void UnsuccessfulPurchaseOperation() throws InterruptedException {
+		printMethodName();
+		driver.get("https://atid.store/");
+		driver.manage().window().setSize(new Dimension(1052, 666));
+		driver.findElement(By.id("menu-item-45")).click();
+		driver.findElement(By.xpath("//*[@id=\"main\"]/div/ul/li[1]/div[1]/a/img")).click();
+		driver.findElement(By.name("add-to-cart")).click();
+		driver.findElement(By.className("ast-site-header-cart")).click();
+		driver.findElement(By.className("wc-proceed-to-checkout")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("place_order")).click();
+		Thread.sleep(3000);
+		// List<WebElement> l = driver.findElements(By.className("woocommerce-error"));
+		if (!driver.findElements(By.className("woocommerce-error")).isEmpty()) {
+			// found at least one error (we wanted to see those error)
+			countUnsuccessfulPurchaseOperation++;
+		}
+	}
+
+	@Test
+	public void SuccessfulAddToCartOperation() throws InterruptedException {
 		printMethodName();
 		driver.get("https://atid.store/");
 		driver.manage().window().setSize(new Dimension(1052, 666));
@@ -132,46 +133,44 @@ public class SanityTests {
 		driver.findElement(By.id("menu-item-45")).click();
 		driver.findElement(By.xpath("//*[@id=\"main\"]/div/ul/li[1]/div[1]/a/img")).click();
 		driver.findElement(By.name("add-to-cart")).click();
-		double productPrice = Double.parseDouble(
-							driver.findElement(By.xpath("//*[@id=\"product-160\"]/div[2]/p/span/bdi")).getText().replaceAll(pattern, ""));
+		double productPrice = Double.parseDouble(driver
+				.findElement(By.xpath("//*[@id=\"product-160\"]/div[2]/p/span/bdi")).getText().replaceAll(pattern, ""));
 		double cartAmountAfterAdd = getCartAmount();
-		System.out.println("Product price: "  + productPrice);
-		System.out.println("Cart before: " + cartAmountBeforeAdd);
-		System.out.println("Cart after: " + cartAmountAfterAdd);
+		// System.out.println("Product price: " + productPrice);
+		// System.out.println("Cart before: " + cartAmountBeforeAdd);
+		// System.out.println("Cart after: " + cartAmountAfterAdd);
 		if (cartAmountBeforeAdd == 0 && cartAmountAfterAdd == productPrice)
 			countSuccessfulAddToCart++;
-			
-	 }
-	
-	
-	//@Test 
-	 public void SuccessfulMultipleAddToCartOperation() throws InterruptedException {
+
+	}
+
+	@Test
+	public void SuccessfulMultipleAddToCartOperation() throws InterruptedException {
 		printMethodName();
 		int count = 2;
 		driver.get("https://atid.store/");
 		driver.manage().window().setSize(new Dimension(1052, 666));
 		double cartAmountBeforeAdd = getCartAmount();
 		for (int i = 0; i < count; i++) {
-			//add the same product two times to the cart
+			// add the same product two times to the cart
 			driver.findElement(By.id("menu-item-45")).click();
 			driver.findElement(By.xpath("//*[@id=\"main\"]/div/ul/li[1]/div[1]/a/img")).click();
 			driver.findElement(By.name("add-to-cart")).click();
 			Thread.sleep(1000);
 		}
-		double productPrice = Double.parseDouble(
-				driver.findElement(By.xpath("//*[@id=\"product-160\"]/div[2]/p/span/bdi")).getText().replaceAll(pattern, ""));
+		double productPrice = Double.parseDouble(driver
+				.findElement(By.xpath("//*[@id=\"product-160\"]/div[2]/p/span/bdi")).getText().replaceAll(pattern, ""));
 		double cartAmountAfterAdd = getCartAmount();
-		System.out.println("Product price: "  + productPrice);
-		System.out.println("Cart before: " + cartAmountBeforeAdd);
-		System.out.println("Cart after: " + cartAmountAfterAdd);
+		// System.out.println("Product price: " + productPrice);
+		// System.out.println("Cart before: " + cartAmountBeforeAdd);
+		// System.out.println("Cart after: " + cartAmountAfterAdd);
 		if (cartAmountBeforeAdd == 0 && cartAmountAfterAdd == productPrice * count)
 			countSuccessfulAddToCart++;
-			
-	 }
-	
-	
-	@Test 
-	 public void SuccessfulProccedToCheckout() throws InterruptedException {
+
+	}
+
+	@Test
+	public void SuccessfulProccedToCheckout() throws InterruptedException {
 		printMethodName();
 		driver.get("https://atid.store/");
 		driver.manage().window().setSize(new Dimension(1052, 666));
@@ -180,75 +179,75 @@ public class SanityTests {
 		driver.findElement(By.name("add-to-cart")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.className("ast-site-header-cart")).click();
-		if(driver.findElement(By.className("wc-proceed-to-checkout")).isDisplayed())
+		if (driver.findElement(By.className("wc-proceed-to-checkout")).isDisplayed())
 			countSuccessfulProccedToCheckout++;
-		
-			
-	 }
 
-	
-	//@Test 
-	 public void SuccessfulCantProccedToCheckout() throws InterruptedException {
+	}
+
+	@Test
+	public void SuccessfulCantProccedToCheckout() throws InterruptedException {
 		printMethodName();
 		driver.get("https://atid.store/");
 		driver.manage().window().setSize(new Dimension(1052, 666));
 		driver.findElement(By.id("menu-item-45")).click();
 		driver.findElement(By.xpath("//*[@id=\"main\"]/div/ul/li[1]/div[1]/a/img")).click();
-		//driver.findElement(By.name("add-to-cart")).click();
+		// driver.findElement(By.name("add-to-cart")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.className("ast-site-header-cart")).click();
-		driver.findElement(By.className("wc-proceed-to-checkout"));
-		//THERE IS BUG HERE
-		
-			
-	 }
-	 
+		try {
+			driver.findElement(By.className("wc-proceed-to-checkout"));
+		} catch (NoSuchElementException e) {
+			// if can't find that element -> the test completed successfully
+			countSuccessfulCantProccedToCheckout++;
+		}
+
+	}
 
 	public static void main(String args[]) {
 		JUnitCore junit = new JUnitCore();
 		junit.addListener(new TextListener(System.out));
 		org.junit.runner.Result result = junit.run(SanityTests.class);
-		
+
 		if (result.getFailureCount() > 0) {
 			System.out.println("Test failed.");
 			System.exit(1);
 		} else {
 			System.out.println("Test finished without errors.");
-			//System.exit(0);
+			// System.exit(0);
 		}
-		
-		if(countSuccessfulPurchaseOperation == 0) {
+
+		if (countSuccessfulPurchaseOperation == 0) {
 			System.out.println("Test of successful purchase failed");
 			System.exit(1);
 		}
-		
-		if(countUnsuccessfulPurchaseOperation == 0) {
+
+		if (countUnsuccessfulPurchaseOperation == 0) {
 			System.out.println("Test of unsuccessful purchase failed");
 			System.exit(1);
 		}
-		
+
 		if (countSuccessfulAddToCart == 0) {
 			System.out.println("Test of successful add to card failed");
 			System.exit(1);
 		}
-		
+
 		if (countSuccessfulMultipleAddToCart == 0) {
 			System.out.println("Test of successful multiple add to card failed");
 			System.exit(1);
 		}
-		
+
 		if (countSuccessfulProccedToCheckout == 0) {
 			System.out.println("Test of successful proceed to checkout failed");
 			System.exit(1);
 		}
-		
+
 		if (countSuccessfulCantProccedToCheckout == 0) {
 			System.out.println("Test of cant proceed to checkout failed");
 			System.exit(1);
 		}
-		
+
 		System.out.println("All tests finished successfully");
 		System.exit(0);
-		
+
 	}
 }
